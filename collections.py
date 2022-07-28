edabf5d34bdfc69c2efd3e94751a44e901fed5f3
Nedudigital <segunchinedu2000@gmail.com>
2022-07-28 01:52:43 +0100

    new

--- collections.py
+++ collections.py
@@ -1,5 +1,1 @@
 '''the collections module implements special data types like the : counter, namedtuple, ordereddict, defaultdict, deque'''
-from collections import Counter
-a = "aaabbbcccc"
-my_counter = Counter(a)
-print(my_counter.items())
